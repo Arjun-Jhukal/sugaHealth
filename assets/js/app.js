f680bba3bc7870699fbf710fba972cbcc0e8ccda
Arjun Jhukal <jhukal9@gmail.com>
2023-03-05 17:30:50 +0545

    Complete News Site #Traced

---
+++ assets/js/app.js
@@ -0,0 +1,64 @@
+// Show and hide search
+var searchIcon = document.querySelector(".search_icon");
+searchIcon.addEventListener("click", () => {
+  var searchBox = document.querySelector(".search_bar");
+  searchBox.classList.add("active");
+});
+
+var closeIcon = document.querySelector(".close_search");
+closeIcon.addEventListener("click", () => {
+  var searchBox = document.querySelector(".search_bar");
+  searchBox.classList.remove("active");
+});
+
+// Back to top and scroll function
+const myBtn = document.getElementById("back_to_top");
+
+window.onscroll = function () {
+  scrollFunction();
+  stickyHeader();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    myBtn.style.display = "block";
+  } else {
+    myBtn.style.display = "none";
+  }
+}
+
+myBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+
+// Scroll function
+
+const header = document.querySelector(".header");
+const menuLogo = document.querySelector(".menu .logo");
+const menuLeft = document.querySelector(".menu_left");
+
+const stickyHeader = () => {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    header.classList.add("sticky_header");
+    menuLeft.classList.add("sticky_active");
+    menuLogo.style.display = "inline-block";
+  } else {
+    header.classList.remove("sticky_header");
+    menuLogo.style.display = "none";
+  }
+};
+
+// Expand and Hide OffCanvas
+// document.onload = () => {
+const hamburgerBtn = document.querySelector(".hamburger");
+var canvas = document.querySelector(".menu_items");
+var closeCanvas = document.querySelector("li.close");
+
+hamburgerBtn.addEventListener("click", () => {
+  canvas.classList.add("expand");
+});
+
+closeCanvas.addEventListener("click", () => {
+  canvas.classList.remove("expand");
+});
+// };
